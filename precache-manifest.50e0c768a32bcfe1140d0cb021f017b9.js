6d0f2f2405d4f034ef9ec1fc32dfd3918f866418
Vishal-9 <gaikwadvishalgv@gmail.com>
2021-03-14 17:19:03 +0530

    Updates

---
+++ precache-manifest.50e0c768a32bcfe1140d0cb021f017b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "722e5a5be2202931402b",
+    "url": "/blockchain-news-sharing/static/js/runtime~main.722e5a5b.js"
+  },
+  {
+    "revision": "39dc204ec38c9be461bc",
+    "url": "/blockchain-news-sharing/static/js/main.39dc204e.chunk.js"
+  },
+  {
+    "revision": "de136e6604848218fd09",
+    "url": "/blockchain-news-sharing/static/js/1.de136e66.chunk.js"
+  },
+  {
+    "revision": "39dc204ec38c9be461bc",
+    "url": "/blockchain-news-sharing/static/css/main.db6ade80.chunk.css"
+  },
+  {
+    "revision": "de136e6604848218fd09",
+    "url": "/blockchain-news-sharing/static/css/1.0bfc8b26.chunk.css"
+  },
+  {
+    "revision": "a776f99067a1a5ad7720743b4f400325",
+    "url": "/blockchain-news-sharing/index.html"
+  }
+];
