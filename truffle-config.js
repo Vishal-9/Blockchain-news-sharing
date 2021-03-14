6af58bc871fc7856f1ded9b3e18ea82d006ed9d6
Vishal-9 <gaikwadvishalgv@gmail.com>
2021-03-15 00:30:48 +0530

    git pages deployed

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,7 @@
+
+var HDWalletProvider = require("@truffle/hdwallet-provider");
+
+var mnemonic = "piano assist attract palm dinner combine oven man light aspect rose trim";
 require('babel-register');
 require('babel-polyfill');
 
@@ -7,6 +11,12 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" 
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/110ff462a51c4fd09ec432636debb8c4"),
+      network_id: 3,
+      gas: 4700000,
+      gasPrice: 100000000000
     },
   },
   contracts_directory: './src/contracts/',
